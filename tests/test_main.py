50142ced469f35ca593c77aace1e455fec0e38fe
ujrc <uwjearc@yahoo.com>
2015-11-06 13:06:26 -0600

    Added tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -31,6 +31,10 @@ class MainTests(unittest.TestCase):
         name=name, password=password), follow_redirects=True)
 
     #Tests
+    def test_index(self):
+        """Ensure flask was set up correctly."""
+        response=self.app.get('/',content_type='html/text')
+        self.assertEqual(response.status_code,200)
 
     def test_404_error(self):
         response = self.app.get('/this-route-does-not-exist/')
