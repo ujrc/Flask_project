8153597519490a066d89c4b501b42078f60211e7
ujrc <uwjearc@yahoo.com>
2015-11-06 13:12:38 -0600

    added tests to test_master

--- tests/test_main.py
+++ tests/test_main.py
@@ -31,7 +31,9 @@ class MainTests(unittest.TestCase):
         name=name, password=password), follow_redirects=True)
 
     #Tests
+
     def test_index(self):
+        
         """Ensure flask was set up correctly."""
         response=self.app.get('/',content_type='html/text')
         self.assertEqual(response.status_code,200)
